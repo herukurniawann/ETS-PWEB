4d9e226aa12689e37b13e0a674388c660b09b11e
Heru Dwi Kurniawan <93961310+herukurniawann@users.noreply.github.com>
2023-10-18 14:51:32 +0700

    Add files via upload

---
+++ template-ets-pweb-d-main/Script.js
@@ -0,0 +1,10 @@
+ShowAll.addEventListener('click', function(){
+    allEmployees();
+
+})
+
+function allEmployees(){
+var xmlhttp = new new XMLHttpRequest();
+var url = "http://it-its.id/api/books";
+xmlhttp.open(url?: DOMStringList, target?: DOMStringMap)
+}
